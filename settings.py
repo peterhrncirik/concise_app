2411c843e2211dc1ba03062b862ed2abbe82d497
Peter Hrnčírik <42555620+peterhrncirik@users.noreply.github.com>
2022-11-04 10:29:44 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-# app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'Optional default value')
 # DB Setup
 db = SQLAlchemy(app)
